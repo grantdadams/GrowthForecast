d48c045380e20b2c88a03967224610ed548bbb17
Grant Adams <adamsgd@uw.edu>
2024-11-27 14:28:59 -0800

    convert fit nn to neuralnet package (RTMB works tho...))

--- R/fit_nn.R
+++ R/fit_nn.R
@@ -1,66 +1,30 @@
-nn_fun <- function(pars){
-  require(RTMB)
-  RTMB::getAll(pars, data_list)
-
-  # Parameter transform ----
-  sigmaObs = exp(log_sigma_obs)
-
-  # Data transform ----
-  logweight = (weight)
-
-  # Model ----
-  hidden = 1/(1 + exp(-mat %*% layer1))
-  for(i in 1:nlayer){
-    hidden = 1/(1 + exp(-(hidden %*% hidden_layer[,,1])))
-  }
-
-  logoutput = hidden %*% last_layer
-  output = (logoutput)
-
-  # Likelihood ----
-  nll = -sum(dnorm(logweight, logoutput, sigmaObs, TRUE))
-
-  # Report ----
-  RTMB::REPORT(sigmaObs)
-  RTMB::REPORT(output)
-
-  return(nll)
-}
 
 
+fit_nn <- function(data,
+                   n_proj_years = 2){
+  require(neuralnet)
 
-fit_nn <- function(dat){
+  # Fit deep NN ----
+  nn <- neuralnet(log(weight) ~ age+year,
+                  data = data,
+                  hidden = c(5,5,5,5),
+                  linear.output = TRUE,
+                  stepmax = 1e6,
+                  lifesign = 'minimal',
+                  rep=1)
 
-  # - Rearrange data
-  hdim = 5
-  nlayer = 5
-  nnform = formula(~-1+age+year)
-  data_list <- list(
-    weight = data$weight,
-    mat = model.matrix(nnform, data),
-    nlayer = nlayer
-  )
+  # Predict ----
+  years <- do.call(seq, as.list(range(data$year)))
+  proj_years <- (max(years) + 1):(max(years) + n_proj_years)
+  ages <- do.call(seq, as.list(range(data$age)))
+  years_ages <- expand.grid(years, ages)
+  colnames(years_ages) <- c("year", "age")
 
-
-  # Parameters ----
-  par_list <- list(
-    layer1   = matrix(0, ncol(data_list$mat), hdim),
-    hidden_layer  = array(0, dim = c(hdim, hdim, nlayer)),
-    last_layer = matrix(0, hdim, 1),
-    log_sigma_obs   = -.3
-  )
+  years_ages$pred_weight <- exp(predict(nn, newdata = years_ages))
 
   # Build and fit ----
-  obj <- MakeADFun(nn_fun, par_list, silent = FALSE)
-  fit <- optim(par = obj$par,
-               fn = obj$fn,
-               gr = obj$gr,
-               control = list(maxit = 1e6))
-  report <- obj$report(obj$env$last.par.best)
-  plot(data$age, report$output)
-  plot(data$weight, report$output)
 
   # Return ----
-  return(list(obj = obj, data = dat, fit = fit, report = report))
+  return(list(obj = nn, data = data, report = years_ages))
 }
 
