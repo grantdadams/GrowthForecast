157013862fad0371dd9d47c4994a69f556af0ab2
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2025-01-22 14:12:07 -0800

    update act_neuron
    need to rename variables for testing

--- R/fit_lstm_tmb.r
+++ R/fit_lstm_tmb.r
@@ -1,10 +1,10 @@
 # LSTM Cell Function ----
-ltsm_neuron <- function(x, h_prev, c_prev, W, U, b) {
-  z <- x %*% W + h_prev %*% U +  matrix(b, nrow = 1,  byrow = TRUE)  # Compute the combined input  # Compute the combined input
-  input_gate <- 1 / (1 + exp(-z[, 1:dim(W)[2]]))  # Compute the input gate
-  forget_gate <- 1 / (1 + exp(-z[, ((ncol(W)/4) + 1):(2 * (ncol(W)/4))]))  # Compute the forget gate
-  output_gate <-1 / (1 + exp(-z[, ((2 * (ncol(W)/4)) + 1):(3 * (ncol(W)/4))])) # Compute the output gate
-  candidate <- tanh(z[, ((3 * (ncol(W)/4)) + 1):(4 * (ncol(W)/4))])  # Compute the candidate cell state
+lstm_neuron <- function(x, h_prev, c_prev, W, U, b) {
+  act_neuron <- x %*% W + h_prev %*% U # Compute the combined input
+  input_gate <- 1 / (1 + exp(-act_neuron[, 1:(ncol(W)/4)]))  # Compute the input gate
+  forget_gate <- 1 / (1 + exp(-act_neuron[, ((ncol(W)/4) + 1):(2 * (ncol(W)/4))]))  # Compute the forget gate
+  output_gate <- 1 / (1 + exp(-act_neuron[, ((2 * (ncol(W)/4)) + 1):(3 * (ncol(W)/4))]))  # Compute the output gate
+  candidate <- tanh(act_neuron[, ((3 * (ncol(W)/4)) + 1):(4 * (ncol(W)/4))])  # Compute the candidate cell state
   c <- forget_gate * c_prev + input_gate * candidate  # Update the cell state
   h <- output_gate * tanh(c)  # Compute the hidden state
   return(list(h = h, c = c))  # Return the hidden and cell states
@@ -23,6 +23,9 @@ lstm_fun_rtmb <- function(data, nhidden_layer = 2, hidden_dim = 5, input_par = N
 
   # Model ----
   # Initialize LSTM parameters
+  # W   =  matrix(0, ncol(data_list$mat), hidden_dim)
+  # U =  matrix(0, ncol(data_list$mat), hidden_dim)
+  # U  = array(0, dim = c(hidden_dim, hidden_dim, nhidden_layer))
   W <- matrix(rnorm(ncol(data_list$mat) * 4 * hidden_dim), ncol = 4 * hidden_dim)  # Initialize weights for input
   U <- matrix(rnorm(hidden_dim * 4 * hidden_dim), ncol = 4 * hidden_dim)  # Initialize weights for hidden state
   b <- rnorm(4 * hidden_dim)  # Initialize biases
@@ -32,18 +35,23 @@ lstm_fun_rtmb <- function(data, nhidden_layer = 2, hidden_dim = 5, input_par = N
   c <- matrix(0, nrow = nrow(data_list$mat), ncol = hidden_dim)  # Initialize cell states
 
 
-  for (t in 1:nrow(data_list$mat)) {  # Loop through each time step
-    lstm_out <- ltsm_neuron(data_list$mat[t, ],
-                          h[t, ],
-                          c[t, ], W, U, b)  # Compute LSTM cell output
-
+  for (i in 1:nrow(data_list$mat)) {
+    lstm_out <- ltsm_neuron(x= data_list$mat[i, ],
+                          h_prev=h[i, ], ## output layer
+                          c_prev=c[i, ], ## long term memory
+                          W,
+                          U,
+                          b)  # Compute LSTM cell output
+    h <- lstm_out$h
+    c <- lstm_out$c  # Update cell state
     # Update cell & hidden states and ensure matrix structure
-    h  <- matrix(lstm_out$h, nrow = nrow(data_list$mat), ncol = hidden_dim)
-    c  <- matrix(lstm_out$c, nrow = nrow(data_list$mat), ncol = hidden_dim)
+    # h  <- matrix(lstm_out$h, nrow = nrow(data_list$mat), ncol = hidden_dim)
+    # c  <- matrix(lstm_out$c, nrow = nrow(data_list$mat), ncol = hidden_dim)
   }
 
   # Final layer
-  logoutput <- h %*% matrix(rnorm(hidden_dim), ncol = 1)  # Compute the final output
+  logoutput <- h %*% matrix(rnorm(hidden_dim), ncol = 1)  # Compute the final outpu
+  # logoutput <- h %*%  matrix(0, nrow = hidden_dim, ncol = 1)  # Compute the final output
   output <- exp(logoutput)  # Apply exponential to the output
 
   # Likelihood
@@ -115,7 +123,7 @@ fit_lstm_rtmb <- function(data, nhidden_layer = 3, hidden_dim = 5, input_par = N
 }
 
 
-test <- fit_lstm_rtmb(data=dat, input_par = NULL)
+test <- fit_lstm_rtmb(data, input_par = NULL)
 
 dat$predicted <- test$report$output
 
