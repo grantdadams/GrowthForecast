88039a4dfe5662959c4a3d84ae0f64703d3ea12e
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2025-02-12 08:39:54 -0800

    increase default layer/dim

--- R/fit_lstm_tmb.r
+++ R/fit_lstm_tmb.r
@@ -63,7 +63,7 @@ lstm_fun_rtmb <- function(pars, data_list) {
 #' @export
 #'
 #' @examples
-fit_lstm_rtmb <- function(data, nhidden_layer = 1, hidden_dim = 1, input_par = NULL){
+fit_lstm_rtmb <- function(data, nhidden_layer = 3, hidden_dim = 5, input_par = NULL){
 
   # - Rearrange data
   nlayer = nhidden_layer + 2
@@ -109,21 +109,6 @@ fit_lstm_rtmb <- function(data, nhidden_layer = 1, hidden_dim = 1, input_par = N
               parList = par_list, input_par = input_par))
 }
 
-
-# lstm_test <- fit_lstm_rtmb(data, input_par = NULL)
-# nn_test <- fit_nn_rtmb(data, input_par = NULL)
-#
-# data$LSTM <- lstm_test$report$output
-# data$NN <- nn_test$report$output
-#
-# data_plot <- reshape2::melt(data, id = c('year','age','weight'))
-#
-# ggplot(data_plot, aes(x = age, y = weight, colour = year, group =  year)) +
-#   geom_line(aes(y = value)) +
-#   geom_point(size = 2) +
-#   scale_color_discrete() +
-#   facet_grid(~variable) +
-#   ggsidekick::theme_sleek()
 
 
 
